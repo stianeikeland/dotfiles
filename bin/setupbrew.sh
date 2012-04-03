af59cbed88a7627986e8f2c7a8f4d040dc364077
Stian Eikeland <stian.eikeland@gmail.com>
2012-04-03 10:36:36 +0200

    Brew init / install + commonly used packages

---
+++ bin/setupbrew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(/usr/bin/curl -fksSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+
+brew update
+brew install ack axel bash bash-completion ddrescue gist git legit mercurial lftp grc mtr multimarkdown netcat ngrep nmap node p7zip rename pv ssh-copy-id unrar di
